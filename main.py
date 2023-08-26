f89a960723830c17e3c14f468c4110056cc968f7
mvuksan544 <mvuksan544@gmail.com>
2023-08-26 10:03:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import pygame as pg
-
+import os
+from pathlib import Path
+source_path = Path(__file__).resolve() #promena directory-a na working directory
+source_dir = source_path.parent
+os.chdir(source_dir)
 pg.init()
